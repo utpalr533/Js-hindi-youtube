2cb1f487f3a155621a2510e0a6419f69ef593346
utpalr533 <127889312+utpalr533@users.noreply.github.com>
2025-09-04 08:38:27 +0000

    strings

--- basic/05_string.js
+++ basic/05_string.js
@@ -1,11 +1,11 @@
-const name = "hitesh"
+const name = "Utpal"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('utpal-hc-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
