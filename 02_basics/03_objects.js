029d6f1c105665e8d42ee90f41b3a5452fa4b770
utpalr533 <127889312+utpalr533@users.noreply.github.com>
2025-09-06 16:51:28 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,17 @@
+// singleton 
+
+// object literals
+// Object.create
+const Jsuser = {
+    name: "Utpal",
+    "full name": "Utpal Rajbongshi",
+    age: 22,
+    location: "Assam",
+    email: "utpal@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+console.log(Jsuser.name)
+console.log(Jsuser["full name"])
+// console.log(Jsuser.full name)
+// myarray = ["U", "t"]
