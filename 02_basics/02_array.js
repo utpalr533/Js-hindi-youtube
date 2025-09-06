1c8817c0c0b04dd41985eca237aabde84275107b
utpalr533 <127889312+utpalr533@users.noreply.github.com>
2025-09-06 15:50:56 +0000

    arrays in dept

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -16,3 +16,10 @@ console.log(real_another_array)
 
 console.log(Array.isArray("Utpal"))
 console.log(Array.from("Utpal"))
+console.log(Array.from({name: "Utpal"})) //Give empty arrays
+
+let score1 = 100
+let score2  = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3))
