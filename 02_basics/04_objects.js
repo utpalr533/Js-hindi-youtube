12b1e7e92e616742c6f927766f35cc5502e0ad51
utpalr533 <127889312+utpalr533@users.noreply.github.com>
2025-09-07 14:16:09 +0000

    objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,49 @@
+// const tinderUser = new object() single ter object
+const tinderUser = {} //non - single tern object
+ tinderUser.id = "123abc"
+ tinderUser.name = "Samay"
+ tinderUser.isLoggedIn = false
+
+//console.log(tinderUser)
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname:"utpal",
+            lastname:"rajbongshi"
+        }
+    }
+}
+//console.log(regularUser.fullname.userfullname.lastname)
+
+const obj1 ={1:"a", 2: "b"}
+const obj2 ={3:"a", 4: "b"}
+//const obj3 = {obj1, obj2}
+//const obj3 = Object.assign({}, obj1, obj2)
+const obj3 = {...obj1, ...obj2}
+//console.log(obj3)
+
+
+const users = [
+    {
+        id:1,
+        email:"utpal@gmail.com"
+    },
+    {
+        id:1,
+        email:"utpal@gmail.com"
+    },
+    {
+        id:1,
+        email:"utpal@gmail.com"
+    }    
+
+]
+users[1].email
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser))
+
+
+console.log(tinderUser.hasOwnProperty('IsLoggedIn'));
