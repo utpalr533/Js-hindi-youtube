ac09f44f9cf1b954522dd21f912ac0764392bc42
utpalr533 <127889312+utpalr533@users.noreply.github.com>
2025-09-07 13:23:27 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,9 +2,13 @@
 
 // object literals
 // Object.create
+
+const mySym = Symbol("key1")
+
 const Jsuser = {
     name: "Utpal",
     "full name": "Utpal Rajbongshi",
+    [mySym]: "mykey1",
     age: 22,
     location: "Assam",
     email: "utpal@google.com",
@@ -13,5 +17,16 @@ const Jsuser = {
 }
 console.log(Jsuser.name)
 console.log(Jsuser["full name"])
+console.log(Jsuser[mySym])
 // console.log(Jsuser.full name)
 // myarray = ["U", "t"]
+
+Jsuser.email = "utpal@chatgpt.com"
+Object.freeze(Jsuser)
+Jsuser.email = "utpal@microsoft.com"
+console.log(Jsuser)
+
+// Jsuser.greeting = function(){
+//     console.log("Hello Javascript user")
+// }
+// console.log(Jsuser.greeting())
