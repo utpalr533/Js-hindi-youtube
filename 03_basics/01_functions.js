d7a2f9488dd47575532bdb1cef20a39ea1bf9873
Utpal Rajbongshi <127889312+utpalr533@users.noreply.github.com>
2025-09-09 15:15:44 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -6,7 +6,25 @@ function sayMyName(){
     console.log("p");
     console.log("a");
     console.log("l");
-
-
 }
-sayMyName()
+//sayMyName()
+
+// function addTwoNumbers(num1, num2){
+//    console.log(num1 + num2)
+// }
+
+function addTwoNumbers(num1, num2){
+  let result = num1 + num2
+  return result
+  console.log("Hello World") // unreachable code;
+  
+} 
+
+addTwoNumbers(5, 7)
+addTwoNumbers(5, "7")
+addTwoNumbers(5, null)
+
+const result = addTwoNumbers(5, 7)
+console.log("Result:", result) //undefined
+
+
