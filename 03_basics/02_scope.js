ee4221d3fbe2d79e0f0f9b8c2ffc2088ac52decc
Utpal Rajbongshi <127889312+utpalr533@users.noreply.github.com>
2025-09-10 05:45:45 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,14 @@
+//var c = 300
+let a = 300
+if (true){
+    let a = 10
+const b = 20
+console.log("INNER: ", a);
+
+for (let i = 0; i<5; i++){
+
+
+}
+console.log(a)
+//console.log("b")
+//console.log(c)
