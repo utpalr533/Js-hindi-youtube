f6a9fa7a7409f96a0817b143e65009f73824f131
Utpal Rajbongshi <127889312+utpalr533@users.noreply.github.com>
2025-09-10 06:08:04 +0000

    scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -5,10 +5,44 @@ if (true){
 const b = 20
 console.log("INNER: ", a);
 
-for (let i = 0; i<5; i++){
-
-
 }
-console.log(a)
-//console.log("b")
-//console.log(c)
+//console.log(a)
+//console.log(b) // not defined
+//console.log(c) // not defined
+function one(){
+    const username = "Utpal"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website) // not defined
+    two()
+    
+}
+//one()
+if(true){
+    const username = "utpal"
+    if(username === "utpal"){
+        const website = "youtube"
+        console.log(username + website);
+    }
+    //console.log(website) // not defined;
+    
+}
+//console.log(username);
+
+// +++++++++++++++++++ intersing +++++++++++++++++++++++
+
+
+function addone(num){
+    return num + 1
+}
+
+// depends on declaration of function 
+//console.log(addone(5))
+
+//addTwo(5) throws error
+const addTwo = function(num){
+        return num + 2
+}// here its declared and hold with a variable
+console.log(addTwo(5))
