e0ae603285596fce54e6485189cf54695dd293d2
Utpal Rajbongshi <127889312+utpalr533@users.noreply.github.com>
2025-09-10 15:15:46 +0000

    switch in js

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -1,55 +1,33 @@
-//if 
-const isUserloggedIN = true;
-const temperature = 30;
-// if(temperature < 40){
-//     console.log("less than 40");
-    
-// }
-// else{
-//     console.log("temperature is greater than 40");
-    
-// }
-//console.log("temperature is greater than 40");
+// //switch (key) {
+//     case value:
+        
+//         break;
 
-
-//if(3 !=2){
-    //console.log("executed");
-
-// 2<2=2
-// <, >, <=,>=,==,===,!=,!==
-
-// const score = 400;
-// if (score > 100) {
-//    let power = "fly";
-//     console.log(`user power: ${power}`);
-// }
-// console.log(`User power: ${power}`);
-
-//const balance = 1000;
-// if (balance > 500) 
-//     console.log("you are eligible to get a credit card"),
-// console.log("hello");
-
-// if (balance <500) {
-//     console.log("less than 500");
-    
-// } else if (balance < 750 ) {
-//     console.log("less than 750");
-// } else if (balance < 900) {
-//     console.log("less than 900");
-// } else {
-//     console.log("less than 1200");
-    
+//     default:
+//         break;
 // }
 
-const UserloggedIN = true;
-const debitcard = true;
-const loggedINFromGoogle = false;
-const loggedINFromFacebook = true;
+const month = 2
+switch (month) {
+    case 1:
+        console.log("January");
+        
+        break;
+    case 2:
+        console.log("feb");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+    case 4:
+        console.log("april");
+        
+        break;
 
-if (UserloggedIN && debitcard && 2==3) {
-    console.log("you can make a purchase")
-}
-if(loggedINFromFacebook || loggedINFromGoogle){
-    console.log("user logged in")
+    default:
+        console.log("not a valid month");
+        
+        break;
 }
