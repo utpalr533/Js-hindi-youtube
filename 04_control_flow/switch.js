efe8339f6b1ea0a3839ec36e0efe23ea19d1fb86
Utpal Rajbongshi <127889312+utpalr533@users.noreply.github.com>
2025-09-10 15:03:03 +0000

    if and else in js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,55 @@
+//if 
+const isUserloggedIN = true;
+const temperature = 30;
+// if(temperature < 40){
+//     console.log("less than 40");
+    
+// }
+// else{
+//     console.log("temperature is greater than 40");
+    
+// }
+//console.log("temperature is greater than 40");
+
+
+//if(3 !=2){
+    //console.log("executed");
+
+// 2<2=2
+// <, >, <=,>=,==,===,!=,!==
+
+// const score = 400;
+// if (score > 100) {
+//    let power = "fly";
+//     console.log(`user power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+//const balance = 1000;
+// if (balance > 500) 
+//     console.log("you are eligible to get a credit card"),
+// console.log("hello");
+
+// if (balance <500) {
+//     console.log("less than 500");
+    
+// } else if (balance < 750 ) {
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1200");
+    
+// }
+
+const UserloggedIN = true;
+const debitcard = true;
+const loggedINFromGoogle = false;
+const loggedINFromFacebook = true;
+
+if (UserloggedIN && debitcard && 2==3) {
+    console.log("you can make a purchase")
+}
+if(loggedINFromFacebook || loggedINFromGoogle){
+    console.log("user logged in")
+}
