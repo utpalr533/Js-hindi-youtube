b2e282c172f6af1353b18d7d05f68b01742036e7
Utpal Rajbongshi <127889312+utpalr533@users.noreply.github.com>
2025-09-10 15:21:43 +0000

    switch

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -6,6 +6,8 @@
 //     default:
 //         break;
 // }
+//break is usually for breaking the controll flow
+//if we dont use break it will execute all the cases after the matched case
 
 const month = 2
 switch (month) {
@@ -22,6 +24,30 @@ switch (month) {
         
         break;
     case 4:
+        console.log("april");
+        
+        break;
+
+    default:
+        console.log("not a valid month");
+        
+        break;
+}
+const month2 = "march"
+switch (month2) {
+    case "january":
+        console.log("January");
+        
+        break;
+    case "feb":
+        console.log("feb");
+        
+        break;
+    case "march":
+        console.log("march");
+        
+        break;
+    case "april":
         console.log("april");
         
         break;
