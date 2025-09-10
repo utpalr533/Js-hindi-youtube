e7363d34240cd0787d746dee694eb5c48dc1469e
Utpal Rajbongshi <127889312+utpalr533@users.noreply.github.com>
2025-09-10 17:00:06 +0000

    truth or false in js

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -16,7 +16,21 @@ if (userEmail) {
 //     console.log("Array is empty"); <---- empty array checking
 // } 
 
-const emptyobj={}
-    if (object.keys(emptyobj).length === 0){
-        console.log("object is empty");
-    }
+//const emptyobj={}
+    //if (object.keys(emptyobj).length === 0){
+        //console.log("object is empty");
+    //}
+//Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 =5 ?? 10
+// var1 =null ?? 10
+//val1 = undefined ?? 10
+val1 =null ?? 10 ?? 20
+console.log(val1);
+
+// Ternary Operator
+//condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? conosole.log("Too expensive") : console.log("Good price");
