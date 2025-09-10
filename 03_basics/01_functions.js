57ae03110d02220600e82370edb87a458d8cf776
Utpal Rajbongshi <127889312+utpalr533@users.noreply.github.com>
2025-09-10 05:25:09 +0000

    functions and objects

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -34,8 +34,41 @@ const result = addTwoNumbers(5, 7)
   }
       return `${username} just logged in`
  }
-console.log(loginUserMessage("Utpal"))
-console.log(loginUserMessage()) //undefined
+// console.log(loginUserMessage("Utpal"))
+// console.log(loginUserMessage()) //undefined
 
 
+function calculaterCartPrice(...num1){
+  return num1
+}
+//console.log(calculaterCartPrice(200, 400, 500))
 
+function calculaterCartPrice2(val1, val2, ...num2){
+  return num2
+}
+//console.log(calculaterCartPrice2(200, 400, 500))
+
+const user ={username: "Utpal", price: 400,}
+ function handleObject(anyObject){
+  console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+ } 
+//handleObject(user)
+ handleObject({username: "Rohan", price: 500})
+ 
+
+ const user2 ={
+  username: "Utpal",
+  prices: 400,
+  }
+ function handleObject(anyObject){
+  console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+ } 
+ handleObject(user2)
+
+ const myNewArray = [200, 400, 500, 600, 7]
+
+  function returnSecondValue(getArray){
+    return getArray[1]
+  }
+ console.log(returnSecondValue(myNewArray));
+ 
