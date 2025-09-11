95f64ba0651fd2718011558892088c7ce62792e7
Utpal Rajbongshi <127889312+utpalr533@users.noreply.github.com>
2025-09-11 08:52:05 +0000

    unique iteration

---
+++ 05_iterations/three.js
@@ -0,0 +1,37 @@
+// for of 
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr =[1,2,3,4,5]
+for (const num of arr){
+    //console.log(num);
+}
+
+
+const greetings = "Hello World"
+for (const greet of greetings){
+    //console.log(`Each char is ${greet}`);
+}
+
+// Maps 
+
+const map = new Map()
+map.set('INR', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+//console.log(map);
+
+for (const [key, value] of map){
+    console.log(key, ':-', value);
+    
+}
+
+// const myObject ={
+//     'game1': 'NFS',
+//     'game2': 'Call of Duty'
+ // MY OBJECT IS NOT ITERABLE
+// }
+// for (const [key, value] of myObject)
+//     console.log(key, ':-', value);
+    
