a4fafcf0348b7da6d2032bbf74b4d05e09aa7bf0
Utpal Rajbongshi <127889312+utpalr533@users.noreply.github.com>
2025-09-11 06:23:29 +0000

    while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,28 @@
+
+// let index = 0;
+// while(index <=10){
+//  console.log(`Value of index is ${index}`);
+//  index = index +2;
+ 
+// }
+
+let myArray = ["Flash", "Arrow", "Superman", "Batman", ]
+let arr = 0;
+while (arr < myArray.length) {
+   // console.log(`Value is ${myArray[arr]}`);
+    arr =arr +1;
+    
+}
+let score =11
+do{
+    console.log(`Value of score is ${score}`);
+    score++
+    
+} while(score <=10)
+    
+let score2 =1
+do{
+    console.log(`Value of score is ${score2}`);
+    score2++
+    
+} while(score2 <=10)
