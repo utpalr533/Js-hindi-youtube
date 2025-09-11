95a88f27c1c9fdcc713de1309ebdf42af460756e
Utpal Rajbongshi <127889312+utpalr533@users.noreply.github.com>
2025-09-11 05:59:16 +0000

    for loops

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,7 +1,51 @@
 // for loop
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if(element ==5){
+        //console.log("5 is my favorite number");
+        
+    }
+    //console.log(element);
+}
+
+for (let i =0; i <= 10; i++){
+    //console.log(`Outer loop value: ${i}`);
+    
+    for (let j = 0; j <= 10; j++) {
+       // console.log(`Inner loop value: ${j}`);
+        
+        //console.log(i + '*' + j + '=' + (i*j) );
+        
+        
+    }
+}
+
+let myArray = ["Flash", "Arrow", "Superman", "Batman", ]
+    console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+}
+    
+// break and continue
+// for (let index = 1; index <= 20; index++) {
+//     if(index == 5){
+//         console.log("5 is my favorite number");
+//         break
+        
+//     }
+//     console.log(`Value of i is ${index}`);
+    
+    
+// }
+for (let index = 1; index <= 20; index++) {
+    if(index == 5){
+        console.log("5 is my favorite number");
+        continue
+        
+    }
+    console.log(`Value of i is ${index}`);
+    
     
 }
-console.log();
