3b1be8a677ce292c5cb95895c81eac3106167ab7
Utpal Rajbongshi <127889312+utpalr533@users.noreply.github.com>
2025-09-13 05:21:03 +0000

    filters and map disc

---
+++ 05_iterations/seven.js
@@ -0,0 +1,6 @@
+const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+//const newNums = myNewnumbers.map( (num) => { return num + 10})
+const newNums = myNumbers.map((num) => num * 10)
+.map((num) => num + 1)
+console.log( newNums );
