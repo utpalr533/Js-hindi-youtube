2876e6f57d62a1a950527054c72025e2eece12c7
Utpal Rajbongshi <127889312+utpalr533@users.noreply.github.com>
2025-09-13 05:11:47 +0000

    loops disc

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -10,9 +10,9 @@
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const newNums = myNums.filter( (num) => {
-//     return num > 4
-// } )
+//const newNums = myNums.filter( (num) => {
+   // return num > 4
+//} )
 
 // const newNums = []
 
