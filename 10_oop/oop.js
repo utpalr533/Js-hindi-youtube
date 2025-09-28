c3e01b56148059eb799bdf56f839761209d26974
Utpal Rajbongshi <127889312+utpalr533@users.noreply.github.com>
2025-09-28 06:52:52 +0000

    Object OOp

---
+++ 10_oop/oop.js
@@ -0,0 +1,42 @@
+const user = {
+    username: "Utpal",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        console.log("Got user details from database");
+        console.log(this);
+        
+        
+    }
+}
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(`Username: ${this.username}`);
+
+// const promiseOne = new Promise()
+// const date = new Date()
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function (){
+        console.log(`Welcome $(this.username)`);
+        
+    }
+
+    //return this
+} 
+
+//new creates an empty objects call as instance 
+//construction function is created newly 
+const userOne = new User("Utpal", 12, true)
+const userTwo =new User("ChaiAUrcode", 11, false)
+console.log(userOne);
+console.log(userTwo);
+
+
+
+
